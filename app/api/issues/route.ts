c87d6dac51e9aca4b80b2ae0ad019f062ce5e66e
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-10-21 15:31:40 -0700

    Built an API for creating issues

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,8 +1,9 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+import prisma from "@/prisma/client";
 
-// for larger apps, call it createIssueSchema
-const schema = z.object({
+// for larger apps, call it createIssueSchema or simple schema
+const createIssueSchema = z.object({
   title: z.string().min(1).max(255),
   description: z.string().min(1).max(50),
   status: z.enum(["open", "closed"]),
@@ -10,9 +11,13 @@ const schema = z.object({
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const validation = createIssueSchema.parse(body);
-  if (!validation.success) {
-    return NextResponse.badRequest(validation.error);
-  }
-  const { title, description, status } = body;
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
 }
